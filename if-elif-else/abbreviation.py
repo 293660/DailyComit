8db6c7a1f765d668736850092114bc8bb24c411d
293660 <grandhineela10@gmail.com>
2021-04-24 15:10:56 +0530

    abbreviation

---
+++ if-elif-else/abbreviation.py
@@ -0,0 +1,11 @@
+s=input("enter a word: ")
+if(s == "lol"):
+    print("laughing out loud")
+elif(s == "rolf"):
+    print("rolling on the floor laughing")
+elif(s == "lmk"):
+    print("let me know")
+elif(s == "smh"):
+    print("shaking my head")
+else:
+    print("No abbreviation available")
